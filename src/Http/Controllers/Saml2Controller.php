ec7139da16a12140e97aa06a27325c4c7c6bf085
Artem <brezzhnev@gmail.com>
2019-01-26 16:03:01 +0200

    Saml2Controller formatting

--- src/Http/Controllers/Saml2Controller.php
+++ src/Http/Controllers/Saml2Controller.php
@@ -7,52 +7,66 @@ use Slides\Saml2\Auth;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 
-
+/**
+ * Class Saml2Controller
+ *
+ * @package Slides\Saml2\Http\Controllers
+ */
 class Saml2Controller extends Controller
 {
-
-    protected $saml2Auth;
+    /**
+     * The authentication handler.
+     *
+     * @var Auth
+     */
+    protected $auth;
 
     /**
-     * @param Auth $saml2Auth injected.
+     * Saml2Controller constructor.
+     *
+     * @param Auth $auth
      */
-    function __construct(Auth $saml2Auth)
+    public function __construct(Auth $auth)
     {
-        $this->saml2Auth = $saml2Auth;
+        $this->auth = $auth;
     }
 
-
     /**
-     * Generate local sp metadata
-     * @return \Illuminate\Http\Response
+     * Render the metadata.
+     *
+     * @return \Illuminate\Support\Facades\Response
      */
     public function metadata()
     {
-
-        $metadata = $this->saml2Auth->getMetadata();
+        $metadata = $this->auth->getMetadata();
 
         return response($metadata, 200, ['Content-Type' => 'text/xml']);
     }
 
     /**
-     * Process an incoming saml2 assertion request.
-     * Fires 'Saml2LoginEvent' event if a valid user is Found
+     * Process the SAML Response sent by the IdP.
+     *
+     * Fires "SignedIn" event if a valid user is found.
+     *
+     * @return \Illuminate\Support\Facades\Redirect
      */
     public function acs()
     {
-        $errors = $this->saml2Auth->acs();
+        $errors = $this->auth->acs();
 
         if (!empty($errors)) {
-            logger()->error('Saml2 error_detail', ['error' => $this->saml2Auth->getLastErrorReason()]);
-            session()->flash('saml2_error_detail', [$this->saml2Auth->getLastErrorReason()]);
+            logger()->error('Saml2 error_detail', ['error' => $this->auth->getLastErrorReason()]);
+            session()->flash('saml2_error_detail', [$this->auth->getLastErrorReason()]);
 
             logger()->error('Saml2 error', $errors);
             session()->flash('saml2_error', $errors);
+
             return redirect(config('saml2.errorRoute'));
         }
-        $user = $this->saml2Auth->getSaml2User();
 
-        event(new SignedIn($user, $this->saml2Auth));
+        $user = $this->auth->getSaml2User();
+
+        event(new SignedIn($user, $this->auth));
 
         $redirectUrl = $user->getIntendedUrl();
 
@@ -65,13 +79,18 @@ class Saml2Controller extends Controller
     }
 
     /**
-     * Process an incoming saml2 logout request.
+     * Process the SAML Logout Response / Logout Request sent by the IdP.
+     *
      * Fires 'saml2.logoutRequestReceived' event if its valid.
+     *
      * This means the user logged out of the SSO infrastructure, you 'should' log him out locally too.
+     *
+     * @return \Illuminate\Support\Facades\Redirect
      */
     public function sls()
     {
-        $error = $this->saml2Auth->sls(config('saml2.retrieveParametersFromServer'));
+        $error = $this->auth->sls(config('saml2.retrieveParametersFromServer'));
+
         if (!empty($error)) {
             throw new \Exception("Could not log out");
         }
@@ -80,24 +99,26 @@ class Saml2Controller extends Controller
     }
 
     /**
-     * This initiates a logout request across all the SSO infrastructure.
-     */
-    public function logout(Request $request)
-    {
-        $returnTo = $request->query('returnTo');
-        $sessionIndex = $request->query('sessionIndex');
-        $nameId = $request->query('nameId');
-        $this->saml2Auth->logout($returnTo, $nameId, $sessionIndex); //will actually end up in the sls endpoint
-        //does not return
-    }
-
-
-    /**
-     * This initiates a login request
+     * Initiate a login request.
+     *
+     * @return void
      */
     public function login()
     {
-        $this->saml2Auth->login(config('saml2.loginRoute'));
+        $this->auth->login(config('saml2.loginRoute'));
     }
 
+    /**
+     * Initiate a logout request.
+     *
+     * @return void
+     */
+    public function logout(Request $request)
+    {
+        $this->auth->logout(
+            $request->query('returnTo'),
+            $request->query('nameId'),
+            $request->query('sessionIndex')
+        );
+    }
 }
